ba3adb920fdfdd1eb70a48b409aa781f705fce0b
Nanashikung <nata.station@hotmail.com>
2021-12-17 14:15:05 +0700

    lab11_3.cpp complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    double sum=0,sumS=0;
+    string score;
+    ifstream source("score.txt");
+    while(getline(source,score)){
+        sum += atof(score.c_str());
+        sumS += atof(score.c_str())*atof(score.c_str());
+        count++;
+    }
+    double mean;
+    mean=(sum/count);
+    cout << "Number of data = " << count <<endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sqrt(sumS*(1.0/count)-(mean*mean));
+}
